d7872b87d165e83ab6521ab734913fcc52f8bfc2
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2020-09-16 09:28:45 +0200

    Add project files.

---
+++ TheMaze/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TheMaze
+{
+    class Program
+    {
+        static bool isRunning = true;
+        static void Main(string[] args)
+        {                       
+            while (isRunning == true)
+            {
+                Console.WriteLine("Welcome To The Maze!");
+            }
+        }
+    }
+}
